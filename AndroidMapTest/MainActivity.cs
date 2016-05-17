402939f58990dc47f7f0ea175d7fbf94ac09e006
LonghronShen <LonghronShen@Hotmail.com>
2016-05-18 01:00:32 +0800

    add doc

--- AndroidMapTest/MainActivity.cs
+++ AndroidMapTest/MainActivity.cs
@@ -2,76 +2,87 @@ using Android.App;
 using Android.OS;
 using Com.Baidu.Mapapi;
 using Com.Baidu.Mapapi.Map;
-using Com.Baidu.Platform.Comapi.Basestruct;
 using Android.Graphics.Drawables;
 using Android.Graphics;
 
-namespace BaiduMapTest {
+namespace BaiduMapTest
+{
 
-	[Activity(Label = "BaiduMapTest", MainLauncher = true)]
-	public class MainActivity : Activity {
+    [Activity(Label = "BaiduMapTest", MainLauncher = true)]
+    public class MainActivity
+        : Activity
+    {
 
-		BMapManager mapManager;
-		MapView mapView;
+        BMapManager mapManager;
+        MapView mapView;
 
-		protected override void OnCreate(Bundle savedInstanceState) {
-			this.SetTheme(Android.Resource.Style.ThemeHolo);
-			base.OnCreate(savedInstanceState);
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            this.SetTheme(Android.Resource.Style.ThemeHolo);
+            base.OnCreate(savedInstanceState);
 
-			mapManager = new BMapManager(this.Application);
-			mapManager.Init(GetString(Resource.String.baidu_map_api_key), null);
+            mapManager = new BMapManager();
+            BMapManager.Init();
+            //mapManager.Init(GetString(Resource.String.baidu_map_api_key), null);
 
-			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
 
-			mapView = FindViewById<MapView>(Resource.Id.mapview);
-			mapView.SetBuiltInZoomControls(true);
-			mapView.Satellite = true;
+            mapView = FindViewById<MapView>(Resource.Id.mapview);
+            mapView.ShowZoomControls(true);
+            mapView.Satellite = true;
 
-			var mapController = mapView.Controller;
-			var point = new GeoPoint((int)(39.915 * 1E6), (int)(116.404 * 1E6));
-			mapController.SetCenter(point);
-			mapController.SetZoom(12);
-			addGroundOverlay();
-		}
+            var mapController = mapView.Controller;
+            var point = new GeoPoint((int)(39.915 * 1E6), (int)(116.404 * 1E6));
+            mapController.SetCenter(point);
+            mapController.SetZoom(12);
+            addGroundOverlay();
+        }
 
-		private void addGroundOverlay() {
-			GroundOverlay overlay = new GroundOverlay(mapView);
-			GeoPoint leftBottom = new GeoPoint((int)(39.815 * 1E6), (int)(116.304 * 1E6));
-			GeoPoint rightUp = new GeoPoint((int)(39.999 * 1E6), (int)(116.504 * 1E6));
-			Drawable d = Resources.GetDrawable(Resource.Drawable.Icon);
-			Bitmap bitmap = ((BitmapDrawable)d).Bitmap;
-			Ground ground = new Ground(bitmap, leftBottom, rightUp);
+        private void addGroundOverlay()
+        {
+            GroundOverlay overlay = new GroundOverlay(mapView);
+            GeoPoint leftBottom = new GeoPoint((int)(39.815 * 1E6), (int)(116.304 * 1E6));
+            GeoPoint rightUp = new GeoPoint((int)(39.999 * 1E6), (int)(116.504 * 1E6));
+            Drawable d = Resources.GetDrawable(Resource.Drawable.Icon);
+            Bitmap bitmap = ((BitmapDrawable)d).Bitmap;
+            Ground ground = new Ground(bitmap, leftBottom, rightUp);
 
-			mapView.Overlays.Add(overlay);
-			overlay.AddGround(ground);
-		}
+            mapView.Overlays.Add(overlay);
+            overlay.AddGround(ground);
+        }
 
-		protected override void OnDestroy() {
-			mapView.Destroy();
-			if (mapManager != null) {
-				mapManager.Destroy();
-				mapManager = null;
-			}
-			base.OnDestroy();
-		}
+        protected override void OnDestroy()
+        {
+            mapView.Destroy();
+            if (mapManager != null)
+            {
+                mapManager.Destroy();
+                mapManager = null;
+            }
+            base.OnDestroy();
+        }
 
-		protected override void OnPause() {
-			mapView.OnPause();
-			if (mapManager != null) {
-				mapManager.Stop();
-			}
-			base.OnPause();
-		}
+        protected override void OnPause()
+        {
+            mapView.OnPause();
+            if (mapManager != null)
+            {
+                mapManager.Stop();
+            }
+            base.OnPause();
+        }
 
-		protected override void OnResume() {
-			mapView.OnResume();
-			if (mapManager != null) {
-				mapManager.Start();
-			}
-			base.OnResume();
-		}
-	}
+        protected override void OnResume()
+        {
+            mapView.OnResume();
+            if (mapManager != null)
+            {
+                mapManager.Start();
+            }
+            base.OnResume();
+        }
+    }
 }
 
 
